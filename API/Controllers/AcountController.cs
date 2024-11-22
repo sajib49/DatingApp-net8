3ea7b4834e5ccc9db0161034a70698e5e1ec5cba
MD SAIDUJJAMAN SAJIB <saidujjaman.sajib@valmetpartners.com>
2024-11-22 19:25:43 +0600

    Toekn service added

--- API/Controllers/AcountController.cs
+++ API/Controllers/AcountController.cs
@@ -39,6 +39,31 @@ namespace API.Controllers
             return user;
        }
 
+       [HttpPost("login")]
+       public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+       {
+            var user = await context.Users.SingleOrDefaultAsync(x=>
+                x.UserName == loginDto.UserName.ToLower()
+            ); 
+
+            if(user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac= new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0; i<computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
+            return user;
+       }
+
        private async Task<bool> IsUserExist(string userName)
        {
             return await context.Users.AnyAsync(x=> x.UserName.ToLower() == userName.ToLower());
